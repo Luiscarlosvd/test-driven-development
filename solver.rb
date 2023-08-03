137e13c5942d30249a96c622493ffce356a90fb1
Luiscarlosvd <lc26cr7@gmail.com>
2023-08-03 12:50:05 -0400

    Add fizzbuzz method in solver class

--- solver.rb
+++ solver.rb
@@ -1,14 +1,22 @@
 class Solver
+  def factorial(number)
+    raise 'Error: number must be positive' if number.negative?
 
-  def factorial(number) 
-    if number < 0
-      raise "Error: number must be positive"
-    end
     total = 1
-    for i in 1..number do
-      total = total * i
-    end 
+    (1..number).each do |i|
+      total *= i
+    end
     total
   end
 
+  def fizzbuzz(number)
+    return 'fizzbuzz' if (number % 3).zero? && (number % 5).zero?
+    return 'fizz' if (number % 3).zero?
+    return 'buzz' if (number % 5).zero?
+
+    number.to_s
+  end
 end
+
+solver = Solver.new
+puts solver.fizzbuzz(3)
